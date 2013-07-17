7d8a86f6caa72adf0b5ef441c7c7453ba183c9e4
KocsisDaniel <daniel.kocsis@liferay.com>
2013-07-17 15:45:02 +0200

    LPS-34218 Create deletion system events to be able to export them later

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/impl/CalendarResourceLocalServiceImpl.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/impl/CalendarResourceLocalServiceImpl.java
@@ -25,12 +25,14 @@ import com.liferay.calendar.util.PortletPropsValues;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.lar.ExportImportThreadLocal;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.ResourceConstants;
+import com.liferay.portal.model.SystemEventConstants;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.util.PortalUtil;
@@ -130,6 +132,9 @@ public class CalendarResourceLocalServiceImpl
 	}
 
 	@Override
+	@SystemEvent(
+		action = SystemEventConstants.ACTION_SKIP,
+		type = SystemEventConstants.TYPE_DELETE)
 	public CalendarResource deleteCalendarResource(
 			CalendarResource calendarResource)
 		throws PortalException, SystemException {
