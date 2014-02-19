7c6034e59bab8e104e5eb1cb9fc9635382fb2740
Michael Han <michael.han@liferay.com>
2014-02-19 00:17:06 -0600

    LPS-44380 add other calendar booking statuses as valid for export

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/lar/CalendarBookingStagedModelDataHandler.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/lar/CalendarBookingStagedModelDataHandler.java
@@ -18,6 +18,7 @@ import com.liferay.calendar.model.Calendar;
 import com.liferay.calendar.model.CalendarBooking;
 import com.liferay.calendar.model.CalendarBookingConstants;
 import com.liferay.calendar.service.CalendarBookingLocalServiceUtil;
+import com.liferay.calendar.workflow.CalendarBookingWorkflowConstants;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.lar.BaseStagedModelDataHandler;
@@ -27,6 +28,7 @@ import com.liferay.portal.kernel.lar.PortletDataException;
 import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
 import com.liferay.portal.kernel.trash.TrashHandler;
 import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.service.ServiceContext;
 
@@ -65,6 +67,16 @@ public class CalendarBookingStagedModelDataHandler
 	@Override
 	public String getDisplayName(CalendarBooking calendarBooking) {
 		return calendarBooking.getTitleCurrentValue();
+	}
+
+	@Override
+	public int[] getExportableStatuses() {
+		return new int[] {
+			CalendarBookingWorkflowConstants.STATUS_APPROVED,
+			CalendarBookingWorkflowConstants.STATUS_DENIED,
+			CalendarBookingWorkflowConstants.STATUS_MAYBE,
+			CalendarBookingWorkflowConstants.STATUS_PENDING
+		};
 	}
 
 	@Override
@@ -234,6 +246,5 @@ public class CalendarBookingStagedModelDataHandler
 			validateExport(portletDataContext, stagedModel);
 		}
 	}
-
 
 }
