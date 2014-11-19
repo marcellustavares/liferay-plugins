b1b87c4763005bde3dbd4d08f0f62cfd80953e9f
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2014-11-19 10:35:01 -0300

    LPS-51402 Regenerate

--- portlets/calendar-portlet/docroot/WEB-INF/service/com/liferay/calendar/service/persistence/CalendarBookingFinder.java
+++ portlets/calendar-portlet/docroot/WEB-INF/service/com/liferay/calendar/service/persistence/CalendarBookingFinder.java
@@ -17,7 +17,7 @@ package com.liferay.calendar.service.persistence;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * @author Eduardo Lundgren
+ * @generated
  */
 @ProviderType
 public interface CalendarBookingFinder {
